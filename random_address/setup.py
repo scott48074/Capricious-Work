271d4cf841bd086ea28a6347016fdd46530a993c
Scott <scott48074@users.noreply.github.com>
2017-03-12 10:15:26 -0500

    Moved random-addresses project to it's own repository :)

--- random_address/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='random_address',
-    version='1.0',
-    packages=['scripts'],
-    license='Unlicense',
-    long_description=open('README.txt').read(),
-)
